17c25a44661025caf2e306e0500fce0943c7660b
recai.huseyin@oredata.com <Oredata123//>
2019-09-18 17:06:05 +0300

    add logger

--- src/main/java/userevents/MainApp.java
+++ src/main/java/userevents/MainApp.java
@@ -7,12 +7,16 @@ import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.core.fs.FileSystem;
 import util.Utils;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import static util.Constants.*;
 
 public class MainApp {
 
-    public static void main(String[] args) {
+    private static final Logger logger = Logger.getLogger(MainApp.class.getName());
 
+    public static void main(String[] args) {
         try {
             final ParameterTool params = ParameterTool.fromArgs(args);
             final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
@@ -21,19 +25,17 @@ public class MainApp {
             DataSet<Tuple1<String>> lines;
             if (params.has(INPUT_FILE_PARAMETER)) {
 
-                // get input data
                 lines = Utils.readInput(params.get(INPUT_FILE_PARAMETER), env);
 
                 String outputDir = "";
                 if (params.has(OUTPUT_DIR_PARAMETER)) {
                     outputDir = params.get(OUTPUT_DIR_PARAMETER);
                 } else {
-                    System.out.println("output-dir parameter not found, will create output files in the current directory.");
+                    logger.info("output-dir parameter not found, will create output files in the current directory.");
                 }
 
                 UserEventsProcessor processor = new UserEventsProcessor();
 
-                // print the result
                 processor.getAllEvents(lines).writeAsCsv(outputDir + ALL_EVENTS_OUTPUT, ROW_DELIMITER,
                         DELIMITER, FileSystem.WriteMode.OVERWRITE);
                 processor.getProductViews(lines).writeAsCsv(outputDir + PRODUCT_VIEWS_OUTPUT, ROW_DELIMITER,
@@ -45,15 +47,13 @@ public class MainApp {
                 processor.getTopUsers(lines).writeAsCsv(outputDir + TOP_USERS_OUTPUT, ROW_DELIMITER,
                         DELIMITER, FileSystem.WriteMode.OVERWRITE);
 
-                // execute program
                 env.execute("User Events Batch Processing");
 
             } else {
-                System.out.println("input-file parameter not found, exiting program!");
+                logger.info("input-file parameter not found, exiting program!");
             }
         } catch (Exception e) {
-            System.out.println("error while executing flink job!");
-            e.printStackTrace();
+            logger.log(Level.SEVERE, "error while executing flink job!", e);
         }
     }
 }
